83211eac306f41308754a4edb28a7396885a098e
jbradleydev <jacobbradleydev@gmail.com>
2025-03-01 16:59:00 +0000

    boilerplate for apps and structural changes

--- scripts/script.js
+++ scripts/script.js
@@ -50,22 +50,3 @@ if (doc.requestFullscreen) {
 }
 goFullScreen(); 
   
-
-// change name
-const appName = document.getElementById('appnamecont');
-const gridItems = document.querySelectorAll('.grid-item');
-var appTarget = "contacts";
-function ChangeName(name, event){
-    appName.innerText = name;
-    gridItems.forEach(el => el.firstChild.classList.remove('focus'));
-    const clickedItem = event.currentTarget;
-    clickedItem.firstChild.classList.add('focus');
-    appTarget = name.toLowerCase().replace(' ', '');
-}
-gridItems[4].firstChild.classList.add('focus');
-
-//open app
-const plus = document.getElementById('plus');
-plus.addEventListener('click', ()=>{
-    window.location.href = `${appTarget}.html`;
-})
